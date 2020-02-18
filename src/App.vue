eb3d553fad0f71acb8606c2c562f9f8b81a18468
DUBOST Clément <braunschweig-dubost@outlook.fr>
2020-02-18 16:51:50 +0100

    Changement du background suivant l'heure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:style="{backgroundImage: background}">
     <Search v-on:city-name="getCityWeather($event)" />
     <Weather v-if="this.weatherData" :weather="this.weatherData" />
   </div>
@@ -15,13 +15,17 @@ export default {
 
   components: {
     Search,
-    Weather
+    Weather,
   },
 
   data() {
     return {
       weatherData: null,
-      error: null
+      error: null,
+      sunrise: null,
+      sunset: null,
+      date: null,
+      background: "",
     }
   },
 
@@ -29,14 +33,36 @@ export default {
     async getCityWeather(city) {
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&units=metric&appid=942b001a12073846ceb89fb33e10be0b`);
       this.weatherData = response.data;
+    },
+
+    async getSunInfos() {
+      const response = await axios.get("https://api.ipgeolocation.io/astronomy?apiKey=69eb1e46b8244b139eaa411b23306282");
+      const {date, sunrise, sunset} = response.data;
+
+      this.sunrise = new Date(date + " " + sunrise).getTime()/1000;
+      this.sunset = new Date(date + " " + sunset).getTime()/1000;
+      this.date = Math.round(new Date().getTime()/1000);
+      
+      this.backgroundCondition();
+    },
+
+    backgroundCondition() {
+      if(this.date > this.sunrise && this.date < this.sunset) {
+        this.background = 'url(' + require('@/assets/images/day.png') + ')';
+      }else {
+        this.background = 'url(' + require('@/assets/images/night.png') + ')';
+      }
     }
-  }   
+  },
+
+  created() {
+    this.getSunInfos();
+  }  
 };
 </script>
 
 <style lang="scss">
 #app {
-  background-image: url("./assets/images/day.png");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
